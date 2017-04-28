a3bb187eaf4db8b8013d9def297bba24bec7ff33
Roger Xi <roger.xi.1984@gmail.com>
2017-04-28 13:34:34 +0800

    Commit_2017-04-28

--- Interview/LeetCode/Question5.cs
+++ Interview/LeetCode/Question5.cs
@@ -8,5 +8,33 @@ namespace Interview.LeetCode
 {
     class Question5
     {
+        public static void EntryPoint()
+        {
+            (new Question5()).LongestPalindrome("babad");
+        }
+
+        public string LongestPalindrome(string s)
+        {
+            char[] input = s.ToCharArray();
+
+            for (int endIndex = input.Length - 1; endIndex >= 0; endIndex--)
+                for (int startIndex = 0; startIndex <= input.Length - 1; startIndex++)
+                    if (IsPalindrome(input, startIndex, endIndex))
+                        return s.Substring(startIndex, endIndex - startIndex + 1);
+
+            return string.Empty;
+        }
+
+        private bool IsPalindrome(char[] input, int startIndex, int endIndex)
+        {
+            for (int i = startIndex, j = endIndex; i < j; i++, j--)
+                if (input[i] != input[j])
+                    return false;
+
+            if (startIndex == endIndex)
+                return false;
+            else
+                return true;
+        }
     }
 }
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -13,7 +13,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question58.EntryPoint();
+            Question5.EntryPoint();
         }
     }
 }
