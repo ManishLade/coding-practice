bed0e3efde4ec762d6ae6c4be128724d3496e9cc
Roger Xi <roger.xi.1984@gmail.com>
2017-04-29 15:59:53 +0800

    Commit_2017-04-29

---
+++ Interview/LeetCode/Question26.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LeetCode
+{
+    class Question26
+    {
+        public int RemoveDuplicates(int[] nums)
+        {
+            if (nums == null || nums.Length == 0)
+                return 0;
+            else if (nums.Length == 1)
+                return 1;
+
+            int index1 = 0,
+                index2 = 1;
+
+            while (index2 <= nums.Length - 1)
+                if (nums[index1] == nums[index2])
+                    index2++;
+                else
+                    nums[++index1] = nums[index2++];
+
+            return ++index1;
+        }
+    }
+}
