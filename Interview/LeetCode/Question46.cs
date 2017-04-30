364943611b2a953f2e3438a16e63ab24bc0f22cd
Roger Xi <roger.xi.1984@gmail.com>
2017-04-30 09:04:28 +0800

    Commit_2017-04-30

---
+++ Interview/LeetCode/Question46.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LeetCode
+{
+    class Question46
+    {
+        public static void EntryPoint()
+        {
+            (new Question46()).Permute(new int[] { 1, 2, 3 });
+        }
+
+        IList<IList<int>> Permutes = new List<IList<int>>();
+
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            GetPermute(new List<int>(), nums);
+
+            return Permutes;
+        }
+
+        private void GetPermute(List<int> list, int[] nums)
+        {
+            List<int> tempList = null;
+
+            if (list.Count != nums.Length)
+            {
+                for (int i = 0; i <= nums.Length - 1; i++)
+                {
+                    if (!list.Contains(nums[i]))
+                    {
+                        tempList = new List<int>(list);
+                        tempList.Add(nums[i]);
+                        GetPermute(tempList, nums);
+                    }
+                }
+            }
+            else
+                Permutes.Add(list);
+        }
+    }
+}
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -13,7 +13,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question156.EntryPoint();
+            Question46.EntryPoint();
         }
     }
 }
