70f42962c8be1c9e08b2619711a57a2f40fc4677
Roger Xi <roger.xi.1984@gmail.com>
2017-05-17 12:50:11 +0800

    Commit_2017-05-17

---
+++ Interview/Question94.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Interview.Data_Structure;
+
+namespace Interview
+{
+    class Question94
+    {
+        public IList<int> InorderTraversal(BinaryTree.Node root)
+        {
+            List<int> result = new List<int>();
+
+            Traversal(root, result);
+
+            return result;
+        }
+
+        private IList<int> Traversal(BinaryTree.Node root, IList<int> list)
+        {
+            if (root == null)
+                return list;
+
+            Traversal(root.LeftNode, list);
+            list.Add(root.Value);
+            Traversal(root.RightNode, list);
+
+            return list;
+        }
+    }
+}
