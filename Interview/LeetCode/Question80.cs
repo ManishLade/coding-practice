4a2c5d922c26b8ca94493b842563fb8da4c7151c
Roger Xi <roxi@microsoft.com>
2017-06-06 13:12:23 +0800

    Commit_2017-06-06

--- Interview/LeetCode/Question80.cs
+++ Interview/LeetCode/Question80.cs
@@ -6,14 +6,19 @@ using System.Threading.Tasks;
 
 namespace Interview.LeetCode
 {
-    class Question26
+    class Question80
     {
+        public static void EntryPoint()
+        {
+            (new Question80()).RemoveDuplicates(new int[] { 1, 1, 2, 2 });
+        }
+
         public int RemoveDuplicates(int[] nums)
         {
             int index = 0;
 
             foreach (var num in nums)
-                if (index == 0 || num > nums[index - 1])
+                if (index <= 1 || num > nums[index - 2])
                     nums[index++] = num;
 
             return index;
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -13,7 +13,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question200.EntryPoint();
+            Question80.EntryPoint();
         }
     }
 }
