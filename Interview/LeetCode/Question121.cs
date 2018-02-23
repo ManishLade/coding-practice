7a64f02f1e7790db9d129f2f42fa5d947db55448
Roger Xi <roxi@microsoft.com>
2018-02-23 15:40:59 +0800

    Commit_2018-02-23

--- Interview/LeetCode/Question121.cs
+++ Interview/LeetCode/Question121.cs
@@ -8,5 +8,18 @@ namespace Interview.LeetCode
 {
     class Question121
     {
+        public int MaxProfit(int[] prices)
+        {
+            int min = int.MaxValue;
+            int maxProfit = 0;
+
+            foreach (int i in prices)
+                if (i < min)
+                    min = i;
+                else if (i - min > maxProfit)
+                    maxProfit = i - min;
+
+            return maxProfit;
+        }
     }
 }
