1206a91566aee97d5d2e4fe9ec82cd5776d033d6
Roger Xi <roxi@microsoft.com>
2018-03-09 12:54:06 +0800

    Commit_2018-03-09

--- Interview/LeetCode/Question39.cs
+++ Interview/LeetCode/Question39.cs
@@ -15,7 +15,20 @@ namespace Interview.LeetCode
 
         public IList<IList<int>> CombinationSum(int[] candidates, int target)
         {
-            return null;
+            IList<IList<int>> result = new List<IList<int>>();
+
+            SubsetSum(candidates, target, candidates.Length - 1, result);
+
+            return result;
+        }
+
+        public void SubsetSum(int[] candidates, int target, int end, IList<IList<int>> result)
+        {
+            if (target == 0 || end < 0)
+                return;
+
+            SubsetSum(candidates, target, end - 1, result);
+            SubsetSum(candidates, target - candidates[end], end - 1, result);
         }
     }
 }
