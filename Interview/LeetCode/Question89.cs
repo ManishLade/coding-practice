27abc4c9d8c28e21ca246fadf1bd360f4b373b48
Roger Xi <roxi@microsoft.com>
2018-04-03 14:13:28 +0800

    Commit_2018-04-03

--- Interview/LeetCode/Question89.cs
+++ Interview/LeetCode/Question89.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,32 @@ namespace Interview.LeetCode
 {
     class Question89
     {
+        public static void EntryPoint()
+        {
+            (new Question89()).GrayCode(1);
+        }
 
+        public IList<int> GrayCode(int n)
+        {
+            if (n == 0)
+                return null;
+
+            byte[] currentCombination = new byte[n];
+            Hashtable hash = new Hashtable();
+
+            DFS(currentCombination, hash);
+
+            return null;
+        }
+
+        public void DFS (byte[] previousCombination, Hashtable hash)
+        {
+            hash.Add(previousCombination.GetHashCode(), previousCombination);
+
+            //for (int i = 0; i < previousCombination.Length; i++)
+            //{
+            //    previousCombination[i] = ~(previousCombination[i]);
+            //}
+        }
     }
 }
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -13,7 +13,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question238.EntryPoint();
+            Question89.EntryPoint();
         }
     }
 }
