a072657831b6e4c16fb68917bb9b2ed1b8fb93d0
Roger Xi <roxi@microsoft.com>
2018-07-18 17:21:49 +0800

    Commit_2018-07-18

--- Interview/Program.cs
+++ Interview/Program.cs
@@ -13,7 +13,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question187.EntryPoint();            
+            Question147.EntryPoint();            
         }
     }
 }
