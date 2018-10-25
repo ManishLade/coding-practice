8cc470863194edfa38095db6c009266feedf8027
Roger Xi <roxi@microsoft.com>
2018-10-25 22:18:56 +0800

    Commit_2018-10-25

--- Interview/LeetCode/Question684.cs
+++ Interview/LeetCode/Question684.cs
@@ -8,9 +8,33 @@ namespace Interview.LeetCode
 {
     class Question684
     {
+        public static void EntryPoint()
+        {
+            int[,] edges = { { 9, 10 }, { 5, 8 }, { 2, 6 }, { 1, 5 }, { 3, 8 }, { 4, 9 }, { 8, 10 }, { 4, 10 }, { 6, 8 }, { 7, 9 } };
+
+            (new Question684()).FindRedundantConnection(edges);
+        }
+
         public int[] FindRedundantConnection(int[,] edges)
         {
-            return null;
+            int[] visited = new int[edges.GetLength(0)],
+                  result = new int[2];
+
+            for (int i = 0; i < edges.GetLength(0); i++)
+            {
+                if (visited[edges[i, 0] - 1] == 1 && visited[edges[i, 1] - 1] == 1)
+                {
+                    result[0] = edges[i, 0];
+                    result[1] = edges[i, 1];
+                }
+                else
+                {
+                    visited[edges[i, 0]] = 1;
+                    visited[edges[i, 1]] = 1;
+                }
+            }
+
+            return result;
         }
     }
 }
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -11,7 +11,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question279.EntryPoint();
+            Question76.EntryPoint();
         }
     }
 }
