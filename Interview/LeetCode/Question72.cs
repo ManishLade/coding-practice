b884a8bb27c8583b67348fad1d7c4b81531100f2
Roger Xi <roxi@microsoft.com>
2018-10-29 14:05:25 +0800

    Commit_2018-10-29

--- Interview/LeetCode/Question72.cs
+++ Interview/LeetCode/Question72.cs
@@ -8,5 +8,37 @@ namespace Interview.LeetCode
 {
     class Question72
     {
+        public static void EntryPoint()
+        {
+            (new Question72()).MinDistance("horse", "ros");
+        }
+
+        // https://www.youtube.com/watch?v=We3YDTzNXEk&t=0s&list=PLrmLmBdmIlpsHaNTPP_jHHDx_os9ItYXr&index=8
+        // https://github.com/mission-peace/interview/blob/master/src/com/interview/dynamic/EditDistance.java
+        public int MinDistance(String word1, String word2)
+        {
+            int[,] temp = new int[word1.Length + 1, word2.Length + 1];
+
+            for (int i = 0; i < temp.GetLength(0); i++)
+                temp[i, 0] = i;
+
+            for (int i = 0; i < temp.GetLength(1); i++)
+                temp[0, i] = i;
+
+            for (int i = 1; i <= word1.Length; i++)
+                for (int j = 1; j <= word2.Length; j++)
+                    if (word1[i - 1] == word2[j - 1])
+                        temp[i, j] = temp[i - 1, j - 1];
+                    else
+                        temp[i, j] = 1 + min(temp[i - 1, j - 1], temp[i - 1, j], temp[i, j - 1]);
+
+            return temp[word1.Length, word2.Length];
+        }
+
+        private int min(int a, int b, int c)
+        {
+            int l = Math.Min(a, b);
+            return Math.Min(l, c);
+        }
     }
 }
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -11,7 +11,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question28.EntryPoint();
+            Question72.EntryPoint();
         }
     }
 }
