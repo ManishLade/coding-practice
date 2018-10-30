2dc110f498531ef3249364dda385fa542ffb21e0
Roger Xi <roxi@microsoft.com>
2018-10-30 19:05:36 +0800

    Commit_2018-10-30

--- Interview/LeetCode/Question294.cs
+++ Interview/LeetCode/Question294.cs
@@ -8,11 +8,14 @@ namespace Interview.LeetCode
 {
     class Question294
     {
+        // https://leetcode.com/problems/flip-game-ii/discuss/74010/Short-Java-and-Ruby
         public bool CanWin(string s)
         {
+            for (int i = -1; (i = s.IndexOf("++", i + 1)) >= 0;)
+                if (!CanWin(s.Substring(0, i) + "-" + s.Substring(i + 2)))
+                    return true;
 
-
-            return true;
+            return false;
         }
     }
 }
