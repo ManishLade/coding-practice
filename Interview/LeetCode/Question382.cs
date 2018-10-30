a49d20fc9b8caa2d91c512566ee985188726e442
Roger Xi <roxi@microsoft.com>
2018-10-30 18:45:07 +0800

    Commit_2018-10-30

--- Interview/LeetCode/Question382.cs
+++ Interview/LeetCode/Question382.cs
@@ -8,5 +8,41 @@ namespace Interview.LeetCode
 {
     class Question382
     {
+
+    }
+
+    // https://en.wikipedia.org/wiki/Reservoir_sampling
+    // http://blog.jobbole.com/42550/
+    public class SolutionQuestion382
+    {
+        ListNode head;
+        Random random;
+
+        public SolutionQuestion382(ListNode head)
+        {
+            this.head = head;
+            random = new Random();
+        }
+
+        public int GetRandom()
+        {
+            ListNode c = head;
+            int result = c.val;
+            for (int i = 1; c.next != null; i++)
+            {
+                c = c.next;
+                if (random.Next(i + 1) == i)
+                    result = c.val;
+            }
+
+            return result;
+        }
+
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }
     }
 }
