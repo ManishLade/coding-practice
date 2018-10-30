ef25a9eadfda10a5061fb20a49691ba46c3dcb4f
Roger Xi <roxi@microsoft.com>
2018-10-30 22:45:54 +0800

    Commit_2018-10-30

--- Interview/LeetCode/Question96.cs
+++ Interview/LeetCode/Question96.cs
@@ -8,5 +8,21 @@ namespace Interview.LeetCode
 {
     class Question96
     {
+        // https://leetcode.com/problems/unique-binary-search-trees/discuss/31807/AC-clean-Java-DP-solution
+        public int NumTrees(int n)
+        {
+            if (n == 0)
+                return 0;
+
+            int[] dp = new int[n + 1];
+
+            dp[0] = 1;
+
+            for (int i = 1; i <= n; i++)
+                for (int j = 1; j <= i; j++)
+                    dp[i] += dp[j - 1] * dp[i - j];
+
+            return dp[n];
+        }
     }
 }
