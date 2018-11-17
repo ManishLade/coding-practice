33da8ac2733f11cba266ec3823e754aa7794f989
Roger Xi <roxi@microsoft.com>
2018-11-17 21:34:36 +0800

    Commit_2018-11-18

---
+++ Interview/LeetCode/Question81.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LeetCode
+{
+    class Question81
+    {
+        public bool Search(int[] nums, int target)
+        {
+            int low = 0,
+                high = nums.Length - 1,
+                middle = low + (high - low) / 2;
+
+            while (low < high)
+            {
+                if (target < nums[high])
+                    low = middle;
+                else if (target > nums[low])
+                    high = middle;
+            }
+
+            return false;
+        }
+    }
+}
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -11,7 +11,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question832.EntryPoint();
+            Question33.EntryPoint();
         }
     }
 }
