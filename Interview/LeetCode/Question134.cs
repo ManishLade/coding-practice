87870dbf911603ba7035718ea9dc03edc0f5aa67
Roger Xi <roxi@microsoft.com>
2018-12-10 00:31:33 +0800

    Commit_2018-12-10

---
+++ Interview/LeetCode/Question134.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LeetCode
+{
+    class Question134
+    {
+        public int CanCompleteCircuit(int[] gas, int[] cost)
+        {
+            for (int i = 0; i <= gas.Length - 1; i++)
+            {
+
+            }
+
+            return -1;
+        }
+    }
+}
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -11,7 +11,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question60.EntryPoint();
+            Question179.EntryPoint();
         }
     }
 }
