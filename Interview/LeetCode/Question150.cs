a835d173950fad24a1542139c2733c7f1433b2bc
Roger Xi <roxi@microsoft.com>
2018-12-17 08:49:47 +0800

    Commit_2018-12-17

--- Interview/LeetCode/Question150.cs
+++ Interview/LeetCode/Question150.cs
@@ -8,5 +8,37 @@ namespace Interview.LeetCode
 {
     class Question150
     {
+        public int EvalRPN(string[] tokens)
+        {
+            if (tokens == null || tokens.Length == 0)
+                return 0;
+
+            Stack<string> stack = new Stack<string>();
+            string[] operators = new string[] { "+", "-", "*", "/" };
+            int num1 = 0,
+                num2 = 0;
+
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (operators.Contains<string>(tokens[i]))
+                {
+                    num1 = Convert.ToInt32(stack.Pop());
+                    num2 = Convert.ToInt32(stack.Pop());
+
+                    if (tokens[i] == "+")
+                        stack.Push((num2 + num1).ToString());
+                    else if (tokens[i] == "-")
+                        stack.Push((num2 - num1).ToString());
+                    else if (tokens[i] == "*")
+                        stack.Push((num2 * num1).ToString());
+                    else
+                        stack.Push((num2 / num1).ToString());
+                }
+                else
+                    stack.Push(tokens[i]);
+            }
+
+            return Convert.ToInt32(stack.Pop());
+        }
     }
 }
