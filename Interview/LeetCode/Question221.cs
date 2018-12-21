430e2a4834df9b54413acd3fb7cca80532986b21
Roger Xi <roxi@microsoft.com>
2018-12-21 10:55:26 +0800

    Commit_2018-12-21

--- Interview/LeetCode/Question221.cs
+++ Interview/LeetCode/Question221.cs
@@ -8,9 +8,27 @@ namespace Interview.LeetCode
 {
     class Question221
     {
+        // https://leetcode.com/problems/maximal-square/discuss/61935/6-lines-Visual-Explanation-O(mn)
         public int MaximalSquare(char[,] matrix)
         {
-            return 0;
+            if (matrix == null || matrix.GetLength(0) == 0 || matrix.GetLength(1) == 0)
+                return 0;
+
+            int max = 0,
+                n = matrix.GetLength(0),
+                m = matrix.GetLength(1);
+
+            int[,] dp = new int[n + 1, m + 1];
+
+            for (int i = 1; i <= n; i++)
+                for (int j = 1; j <= m; j++)
+                    if (matrix[i - 1, j - 1] == '1')
+                    {
+                        dp[i, j] = Math.Min(dp[i - 1, j - 1], Math.Min(dp[i - 1, j], dp[i, j - 1])) + 1;
+                        max = Math.Max(max, dp[i, j]);
+                    }
+
+            return max * max;
         }
     }
 }
