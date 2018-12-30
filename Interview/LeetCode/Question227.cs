b614aea4b827fb0c70627484f7049174d81e0994
Roger Xi <roxi@microsoft.com>
2018-12-30 20:54:17 +0800

    Commit_2018-12-30

--- Interview/LeetCode/Question227.cs
+++ Interview/LeetCode/Question227.cs
@@ -8,9 +8,43 @@ namespace Interview.LeetCode
 {
     class Question227
     {
+        public static void EntryPoint()
+        {
+            (new Question227()).Calculate("3+2*2");
+        }
+
         public int Calculate(string s)
         {
-            return 0;
+            Stack<int> stack = new Stack<int>();
+            char sign = '+';
+            int temp = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (char.IsDigit(s[i]))
+                    temp = temp * 10 + (s[i] - '0');
+
+                if ((s[i] != ' ' && !char.IsDigit(s[i])) || i == s.Length - 1)
+                {
+                    if (sign == '+')
+                        stack.Push(temp);
+                    else if (sign == '-')
+                        stack.Push(-temp);
+                    else if (sign == '*')
+                        stack.Push(stack.Pop() * temp);
+                    else if (sign == '/')
+                        stack.Push(stack.Pop() / temp);
+
+                    sign = s[i];
+                    temp = 0;
+                }
+            }
+
+            temp = 0;
+            while (stack.Count > 0)
+                temp += stack.Pop();
+
+            return temp;
         }
     }
 }
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -11,7 +11,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question228.EntryPoint();
+            Question227.EntryPoint();
         }
     }
 }
