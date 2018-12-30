7736a025b42595d05d0517baa16e1eb750065b28
Roger Xi <roxi@microsoft.com>
2018-12-30 17:26:32 +0800

    Commit_2018-12-30

---
+++ Interview/LeetCode/Question227.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LeetCode
+{
+    class Question227
+    {
+        public int Calculate(string s)
+        {
+            return 0;
+        }
+    }
+}
