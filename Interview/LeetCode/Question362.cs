f68a5a68081b53d6e91e96d4b6dac623703c9f8b
Roger Xi <roxi@microsoft.com>
2018-12-31 09:54:15 +0800

    Commit_2018-12-31

--- Interview/LeetCode/Question362.cs
+++ Interview/LeetCode/Question362.cs
@@ -9,4 +9,28 @@ namespace Interview.LeetCode
     class Question362
     {
     }
+
+    public class HitCounter
+    {
+        Queue<int> queue = null;
+
+        public HitCounter()
+        {
+            queue = new Queue<int>();
+        }
+
+        public void Hit(int timestamp)
+        {
+
+            queue.Enqueue(timestamp);
+        }
+
+        public int GetHits(int timestamp)
+        {
+            while (queue.Count > 0 && timestamp - queue.Peek() >= 300)
+                queue.Dequeue();
+
+            return queue.Count();
+        }
+    }
 }
