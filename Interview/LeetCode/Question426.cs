1b2ac1c8b590ef5bdb6925cc94a75b6702ee73d1
Roger Xi <roxi@microsoft.com>
2019-01-07 07:48:06 +0800

    Commit_2019-01-07

---
+++ Interview/LeetCode/Question426.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LeetCode
+{
+    class Question426
+    {
+        public Node TreeToDoublyList(Node root)
+        {
+
+        }
+
+        public class Node
+        {
+            public int val;
+            public Node left;
+            public Node right;
+
+            public Node() { }
+            public Node(int _val, Node _left, Node _right)
+            {
+                val = _val;
+                left = _left;
+                right = _right;
+            }
+        }
+    }
+}
