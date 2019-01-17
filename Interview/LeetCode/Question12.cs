096cf2eb0f10c3f90a066d2209f8fbcbf560fac3
Roger Xi <roxi@microsoft.com>
2019-01-17 22:43:50 +0800

    Commit_2019-01-17

--- Interview/LeetCode/Question12.cs
+++ Interview/LeetCode/Question12.cs
@@ -8,16 +8,54 @@ namespace Interview.LeetCode
 {
     class Question12
     {
-        public string IntToRoman(int num)
+        public static void EntryPoint()
         {
-            string result = string.Empty;
-
-            return result;
+            (new Question12()).IntToRoman(89);
         }
 
-        private string Helper()
+        public string IntToRoman(int num)
         {
-            return string.Empty;
+            int count = 0;
+            Stack<string> stack = new Stack<string>();
+            StringBuilder result = new StringBuilder();
+
+            while (num != 0)
+            {
+                if (num % 10 == 9)
+                    stack.Push(count == 0 ? "IX" :
+                               count == 1 ? "XC" :
+                               count == 2 ? "CM" : string.Empty);
+                else if (num % 10 == 4)
+                    stack.Push(count == 0 ? "IV" :
+                               count == 1 ? "XL" :
+                               count == 2 ? "CD" : string.Empty);
+                else if (num % 10 >= 5 && num % 10 < 9)
+                {
+                    for (int i = num % 10 - 5; i > 0; i--)
+                        stack.Push(count == 0 ? "I" :
+                                   count == 1 ? "X" :
+                                   count == 2 ? "C" :
+                                   count == 3 ? "M" : string.Empty);
+
+                    stack.Push(count == 0 ? "V" :
+                               count == 1 ? "L" :
+                               count == 2 ? "D" : string.Empty);
+                }
+                else if (num % 10 > 0 && num % 10 < 5)
+                    for (int i = num % 10; i > 0; i--)
+                        stack.Push(count == 0 ? "I" :
+                                   count == 1 ? "X" :
+                                   count == 2 ? "C" :
+                                   count == 3 ? "M" : string.Empty);
+
+                count++;
+                num /= 10;
+            }
+
+            while (stack.Count != 0)
+                result.Append(stack.Pop());
+
+            return result.ToString();
         }
     }
 }
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -11,7 +11,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question722.EntryPoint();
+            Question12.EntryPoint();
         }
     }
 }
