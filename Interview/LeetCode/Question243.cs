5af961f94f33bbbce25fc9ee719f283851dc3c54
Roger Xi <roger.xi.1984@gmail.com>
2019-03-15 00:19:15 +0800

    Commit_2019-03-15

--- Interview/LeetCode/Question243.cs
+++ Interview/LeetCode/Question243.cs
@@ -8,5 +8,24 @@ namespace Interview.LeetCode
 {
     class Question243
     {
+        public int ShortestDistance(string[] words, string word1, string word2)
+        {
+            int index1 = -1,
+                index2 = -1,
+                result = Int32.MaxValue;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i] == word1)
+                    index1 = i;
+                else if (words[i] == word2)
+                    index2 = i;
+
+                if (index1 != -1 && index2 != -1)
+                    result = Math.Min(result, Math.Abs(index1 - index2));
+            }
+
+            return result;
+        }
     }
 }
