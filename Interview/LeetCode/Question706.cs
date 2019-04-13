f2259c950def2403009b3977dcffae7e92d09838
Roger Xi <roger.xi.1984@gmail.com>
2019-04-13 19:03:38 +0800

    Commit_2019-04-13

--- Interview/LeetCode/Question706.cs
+++ Interview/LeetCode/Question706.cs
@@ -9,4 +9,45 @@ namespace Interview.LeetCode
     class Question706
     {
     }
+
+    public class MyHashMap
+    {
+        private List<int[]> data = null;
+
+        public MyHashMap()
+        {
+            data = new List<int[]>();
+        }
+
+        public void Put(int key, int value)
+        {
+            foreach (var item in data)
+                if (item[0] == key)
+                {
+                    item[1] = value;
+                    return;
+                }
+
+            data.Add(new int[] { key, value });
+        }
+
+        public int Get(int key)
+        {
+            foreach (var item in data)
+                if (item[0] == key)
+                    return item[1];
+
+            return -1;
+        }
+
+        public void Remove(int key)
+        {
+            for (int i = 0; i < data.Count; i++)
+                if (data[i][0] == key)
+                {
+                    data.RemoveAt(i);
+                    return;
+                }
+        }
+    }
 }
