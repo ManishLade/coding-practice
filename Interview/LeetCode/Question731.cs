afe6ae6c7cf5f6fd2d3e862008365a5b5ff35922
Roger Xi <roger.xi.1984@gmail.com>
2019-04-15 13:37:57 +0800

    Commit_2019-04-15

--- Interview/LeetCode/Question731.cs
+++ Interview/LeetCode/Question731.cs
@@ -10,24 +10,29 @@ namespace Interview.LeetCode
     {
     }
 
-    //public class MyCalendarTwo
-    //{
-    //    private Dictionary<int, Set> _dictionary = new Dictionary<int, Set>();
+    public class MyCalendarTwo
+    {
+        private List<int[]> calendar = new List<int[]>(),
+                            overlap = new List<int[]>();
 
-    //    public MyCalendarTwo()
-    //    {
+        public MyCalendarTwo()
+        {
 
-    //    }
+        }
 
-    //    public bool Book(int start, int end)
-    //    {
+        public bool Book(int start, int end)
+        {
+            foreach (var item in overlap)
+                if (start < item[1] && item[0] < end)
+                    return false;
 
-    //    }
+            foreach (var item in calendar)
+                if (start < item[1] && item[0] < end)
+                    overlap.Add(new int[] { Math.Max(item[0], start), Math.Min(item[1], end) });
 
-    //    public class Set
-    //    {
-    //        public int NodeCount = 0;
-    //        public SortedSet<int> Nodes = new SortedSet<int>();
-    //    }
-    //}
+            calendar.Add(new int[] { start, end });
+
+            return true;
+        }
+    }
 }
