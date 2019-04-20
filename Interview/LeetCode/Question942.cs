24213139ff073366431eb7c6a963d2cc99b534c8
Roger Xi <roger.xi.1984@gmail.com>
2019-04-20 21:32:08 +0800

    Commit_2019-04-20

--- Interview/LeetCode/Question942.cs
+++ Interview/LeetCode/Question942.cs
@@ -8,5 +8,23 @@ namespace Interview.LeetCode
 {
     class Question942
     {
+        // https://leetcode.com/problems/di-string-match/solution/
+        public int[] DiStringMatch(string S)
+        {
+            int N = S.Length,
+                lo = 0,
+                hi = N;
+            int[] ans = new int[N + 1];
+
+            for (int i = 0; i < N; ++i)
+                if (S[i] == 'I')
+                    ans[i] = lo++;
+                else
+                    ans[i] = hi--;
+
+            ans[N] = lo;
+
+            return ans;
+        }
     }
 }
