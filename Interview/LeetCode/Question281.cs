228d2f6b3e053db42882a8936c87f56b71447a3e
Roger Xi <roger.xi.1984@gmail.com>
2019-04-21 23:54:53 +0800

    Commit_2019-04-21

--- Interview/LeetCode/Question281.cs
+++ Interview/LeetCode/Question281.cs
@@ -9,4 +9,33 @@ namespace Interview.LeetCode
     class Question281
     {
     }
+
+    public class ZigzagIterator
+    {
+        private Queue<int> _queue = new Queue<int>();
+
+        public ZigzagIterator(IList<int> v1, IList<int> v2)
+        {
+            int length = v1.Count <= v2.Count ? v1.Count : v2.Count;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (i < v1.Count)
+                    this._queue.Enqueue(v1[i]);
+
+                if (i < v2.Count)
+                    this._queue.Enqueue(v2[i]);
+            }
+        }
+
+        public bool HasNext()
+        {
+            return this._queue.Count == 0;
+        }
+
+        public int Next()
+        {
+            return this._queue.Dequeue();
+        }
+    }
 }
