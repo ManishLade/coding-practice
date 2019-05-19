3057da47aa6cb848178abf08f1885fa1ab51c5c4
Roger Xi <roger.xi.1984@gmail.com>
2019-05-19 19:44:17 +0800

    commit_2019-05-19

--- Interview/LeetCode/Question360.cs
+++ Interview/LeetCode/Question360.cs
@@ -8,5 +8,60 @@ namespace Interview.LeetCode
 {
     class Question360
     {
+        public int[] SortTransformedArray(int[] nums, int a, int b, int c)
+        {
+            if (nums == null || nums.Length == 0)
+                return new int[0];
+
+            int[] sorted = new int[nums.Length];
+            int index = a >= 0 ? nums.Length - 1 : 0,
+                i = 0,
+                j = nums.Length - 1,
+                ri = Cal(nums[i], a, b, c),
+                rj = Cal(nums[j], a, b, c);
+
+            while (i <= j)
+                if (a >= 0)
+                {
+                    if (ri > rj)
+                    {
+                        sorted[index--] = ri;
+                        i++;
+                        if (i <= j)
+                            ri = Cal(nums[i], a, b, c);
+                    }
+                    else
+                    {
+                        sorted[index--] = rj;
+                        j--;
+                        if (i <= j)
+                            rj = Cal(nums[j], a, b, c);
+                    }
+                }
+                else
+                {
+                    if (ri < rj)
+                    {
+                        sorted[index++] = ri;
+                        i++;
+                        if (i <= j)
+                            ri = Cal(nums[i], a, b, c);
+                    }
+                    else
+                    {
+                        sorted[index++] = rj;
+                        j--;
+                        if (i <= j)
+                            rj = Cal(nums[j], a, b, c);
+                    }
+                }
+
+            return sorted;
+        }
+
+        public int Cal(int x, int a, int b, int c)
+        {
+            return (a * x * x + b * x + c);
+        }
     }
 }
