aaf66bb25bbdb6f4e1fee298e5645435a8f843f4
Roger Xi <roger.xi.1984@gmail.com>
2019-05-20 22:23:33 +0800

    commit_2019-05-20

---
+++ Interview/LeetCode/Question1010.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LeetCode
+{
+    class Question1010
+    {
+        public int NumPairsDivisibleBy60(int[] time)
+        {
+            int[] c = new int[60];
+            int res = 0;
+
+            foreach (var t in time)
+            {
+                res += c[(60 - t % 60) % 60];
+                c[t % 60] += 1;
+            }
+
+            return res;
+        }
+    }
+}
