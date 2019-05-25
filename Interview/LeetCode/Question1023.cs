d933618a0993e5afe82c4e3f318d906dd4f5f93d
Roger Xi <roger.xi.1984@gmail.com>
2019-05-25 19:10:45 +0800

    commit_2019-05-25

--- Interview/LeetCode/Question1023.cs
+++ Interview/LeetCode/Question1023.cs
@@ -8,5 +8,33 @@ namespace Interview.LeetCode
 {
     class Question1023
     {
+        public IList<bool> CamelMatch(string[] queries, string pattern)
+        {
+            IList<bool> result = new List<bool>();
+
+            foreach (var query in queries)
+            {
+                bool match = true;
+                int pi = 0,
+                    i = 0;
+
+                while (i < query.Length)
+                {
+                    if (pi < pattern.Length && query[i] == pattern[pi])
+                        pi++;
+                    else if (Char.IsUpper(query[i]))
+                    {
+                        match = false;
+                        break;
+                    }
+
+                    i++;
+                }
+
+                result.Add(match && pi == pattern.Length);
+            }
+
+            return result;
+        }
     }
 }
