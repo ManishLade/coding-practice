74c1e8c5988a2c4f9e1df1a60af746f53db30e6d
Roger Xi <roger.xi.1984@gmail.com>
2019-05-26 10:08:34 +0800

    commit_2019-05-26

--- Interview/LeetCode/Question669.cs
+++ Interview/LeetCode/Question669.cs
@@ -8,5 +8,29 @@ namespace Interview.LeetCode
 {
     class Question669
     {
+        public TreeNode TrimBST(TreeNode root, int L, int R)
+        {
+            if (root == null)
+                return root;
+
+            if (root.val > R)
+                return TrimBST(root.left, L, R);
+
+            if (root.val < L)
+                return TrimBST(root.right, L, R);
+
+            root.left = TrimBST(root.left, L, R);
+            root.right = TrimBST(root.right, L, R);
+
+            return root;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
     }
 }
