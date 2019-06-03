b6cb932ab1c59535520d7af20c04744f27fecd67
Roger Xi <roger.xi.1984@gmail.com>
2019-06-03 16:06:33 +0800

    commit_2019-06-03

--- Interview/LeetCode/Question643.cs
+++ Interview/LeetCode/Question643.cs
@@ -8,5 +8,36 @@ namespace Interview.LeetCode
 {
     class Question643
     {
+        public static void EntryPoint()
+        {
+            (new Question643()).FindMaxAverage(new int[] { 4, 2, 1, 3, 3 }, 2);
+        }
+
+        public double FindMaxAverage(int[] nums, int k)
+        {
+            double result = 0,
+                   slidingWindow = 0;
+
+            if (nums.Length <= k)
+            {
+                foreach (var item in nums)
+                    result += item;
+
+                return result / nums.Length;
+            }
+
+            for (int i = 0; i < k; i++)
+                slidingWindow += nums[i];
+
+            result = slidingWindow;
+
+            for (int i = k; i < nums.Length; i++)
+            {
+                slidingWindow = slidingWindow + nums[i] - nums[i - k];
+                result = slidingWindow > result ? slidingWindow : result;
+            }
+
+            return result / k;
+        }
     }
 }
--- Interview/Program.cs
+++ Interview/Program.cs
@@ -11,7 +11,7 @@ namespace Interview
     {
         static void Main(string[] args)
         {
-            Question1019.EntryPoint();
+            Question643.EntryPoint();
         }
     }
 }
