1d4d747fd5d7836731dc9e9c64a117d45d9757c8
Roger Xi <roger.xi.1984@gmail.com>
2019-06-04 17:10:53 +0800

    commit_2019-06-04

--- Interview/LeetCode/Question1038.cs
+++ Interview/LeetCode/Question1038.cs
@@ -8,5 +8,38 @@ namespace Interview.LeetCode
 {
     class Question1038
     {
+        private int _max = 0;
+
+        public TreeNode BstToGst(TreeNode root)
+        {
+            return Helper(root);
+        }
+
+        private TreeNode Helper(TreeNode node)
+        {
+            if (node == null)
+                return null;
+
+            TreeNode rNode = Helper(node.right),
+                     gstNode = new TreeNode(node.val + _max);
+
+            if (gstNode.val > _max)
+                _max = gstNode.val;
+
+            TreeNode lNode = Helper(node.left);
+
+            gstNode.left = lNode;
+            gstNode.right = rNode;
+
+            return gstNode;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
     }
 }
