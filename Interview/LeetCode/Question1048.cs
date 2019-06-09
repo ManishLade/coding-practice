a5569a7dc7ddc626d102fa778214a4a6de618834
Roger Xi <roger.xi.1984@gmail.com>
2019-06-09 11:19:32 +0800

    commit_2019-06-09

--- Interview/LeetCode/Question1048.cs
+++ Interview/LeetCode/Question1048.cs
@@ -8,5 +8,31 @@ namespace Interview.LeetCode
 {
     class Question1048
     {
+        public int LongestStrChain(string[] words)
+        {
+            Dictionary<string, int> map = new Dictionary<string, int>();
+            int res = 0;
+
+            Array.Sort(words, (a, b) => a.Length - b.Length);
+
+            foreach (string word in words)
+            {
+                map[word] = 1;
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    StringBuilder sb = new StringBuilder(word);
+                    sb.Remove(i, 1);
+                    string next = sb.ToString();
+
+                    if (map.ContainsKey(next) && map[next] + 1 > map[word])
+                        map[word] = map[next] + 1;
+                }
+
+                res = Math.Max(res, map[word]);
+            }
+
+            return res;
+        }
     }
 }
--- Interview/LeetCode/Question1049.cs
+++ Interview/LeetCode/Question1049.cs
@@ -8,5 +8,28 @@ namespace Interview.LeetCode
 {
     class Question1049
     {
+        public int LastStoneWeightII(int[] stones)
+        {
+            HashSet<int> sums = new HashSet<int>();
+            sums.Add(0);
+
+            foreach (var stone in stones)
+            {
+                HashSet<int> nextSums = new HashSet<int>();
+
+                foreach (var s in sums)
+                {
+                    if (!nextSums.Contains(s + stone))
+                        nextSums.Add(s + stone);
+
+                    if (!nextSums.Contains(s - stone))
+                        nextSums.Add(s - stone);
+                }
+
+                sums = nextSums;
+            }
+
+            return sums.Where(s => s >= 0).Min();
+        }
     }
 }
