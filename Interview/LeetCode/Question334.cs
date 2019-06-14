63ddfe1234ce10f5d0954a3263ba05767c6cc44a
Roger Xi <roger.xi.1984@gmail.com>
2019-06-14 16:20:58 +0800

    commit_2019-06-14

--- Interview/LeetCode/Question334.cs
+++ Interview/LeetCode/Question334.cs
@@ -8,5 +8,21 @@ namespace Interview.LeetCode
 {
     class Question334
     {
+        // https://leetcode.com/problems/increasing-triplet-subsequence/discuss/79123/4-lines-C++....
+        public bool IncreasingTriplet(int[] nums)
+        {
+            int a = Int32.MaxValue,
+                b = a;
+
+            foreach (var x in nums)
+                if (x <= a)
+                    a = x;
+                else if (x <= b)
+                    b = x;
+                else
+                    return true;
+
+            return false;
+        }
     }
 }
