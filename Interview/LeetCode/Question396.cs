b84b958656052c260372c4b132e5036aa1a1a308
Roger Xi <roger.xi.1984@gmail.com>
2019-06-15 17:49:20 +0800

    commit_2019-06-15

--- Interview/LeetCode/Question396.cs
+++ Interview/LeetCode/Question396.cs
@@ -8,5 +8,32 @@ namespace Interview.LeetCode
 {
     class Question396
     {
+        public int MaxRotateFunction(int[] A)
+        {
+            if (A == null || A.Length == 0)
+                return 0;
+
+            int n = A.Length;
+            int sum = 0;
+            int f0 = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += A[i];
+                f0 += i * A[i];
+            }
+
+            int max = f0;
+
+            for (int k = 1, fkminus1 = f0; k < n; k++)
+            {
+                int fk = fkminus1 + sum - n * A[n - k];
+
+                max = Math.Max(max, fk);
+                fkminus1 = fk;
+            }
+
+            return max;
+        }
     }
 }
