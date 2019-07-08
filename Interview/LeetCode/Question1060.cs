2f059d2fae3053fa8b0ee78e35cc9c28463d2e5f
Roger Xi <roger.xi.1984@gmail.com>
2019-07-08 23:23:40 +0800

    commit_2019-07-08

--- Interview/LeetCode/Question1060.cs
+++ Interview/LeetCode/Question1060.cs
@@ -8,5 +8,24 @@ namespace Interview.LeetCode
 {
     class Question1060
     {
+        public int MissingElement(int[] nums, int k)
+        {
+            int n = nums.Length;
+
+            if (k > missing(n - 1, nums))
+                return nums[n - 1] + k - missing(n - 1, nums);
+
+            int idx = 1;
+
+            while (missing(idx, nums) < k)
+                idx++;
+
+            return nums[idx - 1] + k - missing(idx - 1, nums);
+        }
+
+        int missing(int idx, int[] nums)
+        {
+            return nums[idx] - nums[0] - idx;
+        }
     }
 }
