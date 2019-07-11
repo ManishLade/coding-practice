451d301b70be7c5be257452c138458dd3b72854e
Roger Xi <roger.xi.1984@gmail.com>
2019-07-11 22:13:32 +0800

    commit_2019-07-11

--- Interview/LeetCode/Question1099.cs
+++ Interview/LeetCode/Question1099.cs
@@ -8,5 +8,26 @@ namespace Interview.LeetCode
 {
     class Question1099
     {
+        public int TwoSumLessThanK(int[] A, int K)
+        {
+            int result = Int32.MinValue,
+                i = 0,
+                j = A.Length - 1;
+
+            Array.Sort(A);
+
+            while (i < j)
+            {
+                if (A[i] + A[j] < K && A[i] + A[j] > result)
+                    result = A[i] + A[j];
+
+                if (A[i] + A[j] < K)
+                    i++;
+                else
+                    j--;
+            }
+
+            return result;
+        }
     }
 }
