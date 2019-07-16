27ded3f52dfea172b331baa0021f0f72f1b187b6
Roger Xi <roger.xi.1984@gmail.com>
2019-07-16 11:32:38 +0800

    commit_2019-07-16

--- Interview/DataStructure/AVLTree.cs
+++ Interview/DataStructure/AVLTree.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Interview.Algorithm.Sort
+namespace Interview.DataStructure
 {
-    class TopologySort
+    class AVLTree
     {
     }
 }
--- Interview/LeetCode/Question450.cs
+++ Interview/LeetCode/Question450.cs
@@ -8,5 +8,60 @@ namespace Interview.LeetCode
 {
     class Question450
     {
+        public TreeNode DeleteNode(TreeNode root, int key)
+        {
+            if (root == null)
+                return null;
+
+            if (key > root.val)
+                root.right = DeleteNode(root.right, key);
+            else if (key < root.val)
+                root.left = DeleteNode(root.left, key);
+            else
+            {
+                if (root.left == null && root.right == null)
+                    root = null;
+                else if (root.right != null)
+                {
+                    root.val = Successor(root);
+                    root.right = DeleteNode(root.right, root.val);
+                }
+                else
+                {
+                    root.val = Predecessor(root);
+                    root.left = DeleteNode(root.left, root.val);
+                }
+            }
+
+            return root;
+        }
+
+        public int Successor(TreeNode root)
+        {
+            root = root.right;
+
+            while (root.left != null)
+                root = root.left;
+
+            return root.val;
+        }
+
+        public int Predecessor(TreeNode root)
+        {
+            root = root.left;
+
+            while (root.right != null)
+                root = root.right;
+
+            return root.val;
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
     }
 }
