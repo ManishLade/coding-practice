85e69a47fdcbca77d2d2e77f90a2e47ed11f9d8d
Roger Xi <roger.xi.1984@gmail.com>
2019-07-16 15:05:37 +0800

    commit_2019-07-16

--- Interview/LeetCode/utility.cs
+++ Interview/LeetCode/utility.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Interview.LeetCode
 {
-    class utility
+    public class utility
     {
         public static int[][] ConvertStringToIntArray(string s)
         {
