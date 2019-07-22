fcf33c98120b41c09e4540b23049cab61b4a8049
Roger Xi <roger.xi.1984@gmail.com>
2019-07-22 13:13:28 +0800

    commit_2019-07-22

--- Interview/LeetCode/Question739.cs
+++ Interview/LeetCode/Question739.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,23 @@ namespace Interview.LeetCode
 {
     class Question739
     {
+        public int[] DailyTemperatures(int[] T)
+        {
+            int[] result = new int[T.Length];
+            Stack<Tuple<int, int>> stack = new Stack<Tuple<int, int>>();
+
+            for (int i = 0; i < T.Length; i++)
+            {
+                while (stack.Count > 0 && stack.Peek().Item1 < T[i])
+                {
+                    result[stack.Peek().Item2] = i - stack.Peek().Item2;
+                    stack.Pop();
+                }
+
+                stack.Push(new Tuple<int, int>(T[i], i));
+            }
+
+            return result;
+        }
     }
 }
