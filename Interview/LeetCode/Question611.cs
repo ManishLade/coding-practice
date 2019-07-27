dc9b3e58ac9fdf82cf0b8296692ec0ddac69823d
Roger Xi <roger.xi.1984@gmail.com>
2019-07-28 02:06:24 +0800

    commit_2019-07-28

--- Interview/LeetCode/Question611.cs
+++ Interview/LeetCode/Question611.cs
@@ -8,5 +8,26 @@ namespace Interview.LeetCode
 {
     class Question611
     {
+        public int TriangleNumber(int[] nums)
+        {
+            int count = 0;
+
+            Array.Sort(nums);
+
+            for (int i = 0; i < nums.Length - 2; i++)
+            {
+                int k = i + 2;
+
+                for (int j = i + 1; j < nums.Length - 1 && nums[i] != 0; j++)
+                {
+                    while (k < nums.Length && nums[i] + nums[j] > nums[k])
+                        k++;
+
+                    count += k - j - 1;
+                }
+            }
+
+            return count;
+        }
     }
 }
