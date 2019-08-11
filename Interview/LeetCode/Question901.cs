80d0d63ad12ee257a0a533e9bd18455de2041229
Roger Xi <roger.xi.1984@gmail.com>
2019-08-11 19:33:38 +0800

    commit_2019-08-11

--- Interview/LeetCode/Question901.cs
+++ Interview/LeetCode/Question901.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +11,32 @@ namespace Interview.LeetCode
 {
     class Question901
     {
+    }
+
+    public class StockSpanner
+    {
+        private Stack<int> _stack = new Stack<int>(),
+                           _weight = new Stack<int>();
+
+        public StockSpanner()
+        {
+
+        }
+
+        public int Next(int price)
+        {
+            int weight = 1;
+
+            while (_stack.Count > 0 && _stack.Peek() <= price)
+            {
+                _stack.Pop();
+                weight += _weight.Pop();
+            }
+
+            _stack.Push(price);
+            _weight.Push(weight);
+
+            return weight;
+        }
     }
 }
