c3ea9e1eceaf3061347df76ef160f12921b0486f
Roger Xi <roger.xi.1984@gmail.com>
2019-08-16 11:07:17 +0800

    commit_2019-08-16

--- Interview/LeetCode/Question413.cs
+++ Interview/LeetCode/Question413.cs
@@ -8,5 +8,19 @@ namespace Interview.LeetCode
 {
     class Question413
     {
+        public int NumberOfArithmeticSlices(int[] A)
+        {
+            int[] dp = new int[A.Length];
+            int sum = 0;
+
+            for (int i = 2; i < dp.Length; i++)
+                if (A[i] - A[i - 1] == A[i - 1] - A[i - 2])
+                {
+                    dp[i] = 1 + dp[i - 1];
+                    sum += dp[i];
+                }
+
+            return sum;
+        }
     }
 }
