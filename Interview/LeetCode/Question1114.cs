c78ac4a16338d8c00ba267287c051bb35d5c5391
Roger Xi <roger.xi.1984@gmail.com>
2019-08-19 14:23:26 +0800

    commit_2019-08-19

---
+++ Interview/LeetCode/Question1114.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LeetCode
+{
+    class Question1114
+    {
+
+    }
+
+    public class Foo
+    {
+        int count;
+
+        public Foo()
+        {
+            this.count = 0;
+        }
+
+        public void First(Action printFirst)
+        {
+            // printFirst() outputs "first". Do not change or remove this line.
+            printFirst();
+
+            this.count++;
+        }
+
+        public void Second(Action printSecond)
+        {
+            while (count < 1)
+                System.Threading.Thread.Sleep(1);
+
+            // printSecond() outputs "second". Do not change or remove this line.
+            printSecond();
+
+            this.count++;
+        }
+
+        public void Third(Action printThird)
+        {
+            while (count < 2)
+                System.Threading.Thread.Sleep(1);
+
+            // printThird() outputs "third". Do not change or remove this line.
+            printThird();
+        }
+    }
+}
