05f51083ded6f0a4089b0cd37d61480c39bf81c1
Roger Xi <roger.xi.1984@gmail.com>
2019-08-23 14:03:13 +0800

    commit_2019-08-23

--- Interview/LeetCode/Question48.cs
+++ Interview/LeetCode/Question48.cs
@@ -8,6 +8,27 @@ namespace Interview.LeetCode
 {
     class Question48
     {
+        public void Rotate(int[][] matrix)
+        {
+            int n = matrix.Length;
 
+            for (int i = 0; i < n; i++)
+                for (int j = i; j < n; j++)
+                {
+                    int tmp = matrix[j][i];
+
+                    matrix[j][i] = matrix[i][j];
+                    matrix[i][j] = tmp;
+                }
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n / 2; j++)
+                {
+                    int tmp = matrix[i][j];
+
+                    matrix[i][j] = matrix[i][n - j - 1];
+                    matrix[i][n - j - 1] = tmp;
+                }
+        }
     }
 }
