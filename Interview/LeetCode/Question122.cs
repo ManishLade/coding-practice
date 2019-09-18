a974ba8b66d3eda746caffc4c984955f71af28f1
Roger Xi <roger.xi.1984@gmail.com>
2019-09-18 15:13:08 +0800

    commit_2019-09-18

--- Interview/LeetCode/Question122.cs
+++ Interview/LeetCode/Question122.cs
@@ -8,5 +8,15 @@ namespace Interview.LeetCode
 {
     class Question122
     {
+        public int MaxProfit(int[] prices)
+        {
+            int total = 0;
+
+            for (int i = 0; i < prices.Length - 1; i++)
+                if (prices[i + 1] > prices[i])
+                    total += prices[i + 1] - prices[i];
+
+            return total;
+        }
     }
 }
---
+++ Interview/LeetCode/Question309.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.LeetCode
+{
+    class Question309
+    {
+        public int MaxProfit(int[] prices)
+        {
+
+        }
+    }
+}
