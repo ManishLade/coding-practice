88ae20101545ffe0883bff8f265feac8c4a42c6e
Roger Xi <roger.xi.1984@gmail.com>
2019-11-30 16:23:07 +0800

    commit_2019-11-30

--- Interview/LeetCode/Question1214.cs
+++ Interview/LeetCode/Question1214.cs
@@ -8,5 +8,34 @@ namespace Interview.LeetCode
 {
     class Question1214
     {
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int x) { val = x; }
+        }
+
+        public bool TwoSumBSTs(TreeNode root1, TreeNode root2, int target)
+        {
+            if (root1 == null)
+                return false;
+
+            return FindInTree(root2, target - root1.val) || TwoSumBSTs(root1.left, root2, target) || TwoSumBSTs(root1.right, root2, target);
+        }
+
+        public bool FindInTree(TreeNode root, int value)
+        {
+            if (root == null)
+                return false;
+
+            if (root.val == value)
+                return true;
+
+            if (root.val < value)
+                return FindInTree(root.right, value);
+            else
+                return FindInTree(root.left, value);
+        }
     }
 }
